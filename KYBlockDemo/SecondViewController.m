1640c7669bf3d798d870f2d9a3bca464f4a01e2e
nonoBruce <1064124945@qq.com>
2016-02-02 11:47:02 +0800

    add some examples
    add some examples

--- KYBlockDemo/SecondViewController.m
+++ KYBlockDemo/SecondViewController.m
@@ -25,9 +25,12 @@
     
     
     //
-    [self localVariable];
+//    [self localVariable];
 //    [self staticVariable];
-
+    exampleA();
+//    exampleB();
+//    exampleC();
+//    exampleE();
 }
 -(void) backToIndex{
     //将首页的导航条隐藏掉
@@ -83,6 +86,91 @@
     //这里输出的4，不是103
     NSLog(@"tem = %ld",tmp);
     //这里输出的1，不是100了
+}
+
+
+#pragma mark - 网上常见例子
+////////////////////////////////////////////////////////////////
+//例子1
+void exampleA() {
+    char a = 'A';
+    ^{
+        NSLog(@"%c",a);
+    }();
+}
+
+
+////////////////////////////////////////////////////////////////
+//例子2
+void exampleB_addBlockToArray(NSMutableArray *array) {
+    char b = 'B';
+    [array addObject:^{
+        NSLog(@"%c",b);
+    } ];
+}
+//这个block在非ARC下，分配到stack上，出了scope就会释放
+
+void exampleB() {
+    NSMutableArray *array = [NSMutableArray array];
+    exampleB_addBlockToArray(array);
+    void (^block)() = [array objectAtIndex:0];
+    
+    block();
+}
+
+
+
+////////////////////////////////////////////////////////////////
+//例子3
+void exampleC_addBlockToArray(NSMutableArray *array) {
+    [array addObject:^{
+        NSLog(@"C\n");
+    }];
+}
+void exampleC() {
+    NSMutableArray *array = [NSMutableArray array];
+    exampleC_addBlockToArray(array);
+    
+    void (^block)() = [array objectAtIndex:0];
+    block();
+    
+}
+
+
+
+////////////////////////////////////////////////////////////////
+//例子4
+//在非ARC情况下报错，提示 返回的是一个local StackBlock，返回后就会被销毁
+//在ARC会默认copy
+
+//typedef void (^dBlock)();
+//dBlock exampleD_getBlock() {
+//    char d = 'D';
+//    return ^{
+//        printf("%c\n", d);
+//    };
+//}
+//void exampleD() {
+//    exampleD_getBlock()();
+//}
+
+////////////////////////////////////////////////////////////////
+//例子5
+
+typedef void (^eBlock)();
+eBlock exampleE_getBlock() {
+    char e = 'E';
+    void (^block)() = ^{
+        printf("%c\n", e);
+    };
+    return block;
+//    提示：Address of stack-allocted block declared on line 113 returned to caller;
+//    这个表示出了作用域（scope）就会失效（自动释放）
+//    return [[block copy] autorelease];
+}
+void exampleE() {
+    eBlock block = exampleE_getBlock();
+    block();
 }
 
 @end
